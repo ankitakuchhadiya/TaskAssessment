a6176fe9e3b649ecc63b57c49873a4e4380bcac3
ankitakuchhadiya <ankitakuchhadiya@gmail.com>
2024-07-05 16:38:02 +0545

    Support different delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,16 @@ function stringCal(numbers) {
     if (numbers === "") {
         return 0;
     }
-    const numberArr = numbers.split(/[\n,]/).map(num => parseInt(num, 10));
+    let delimiter = /[\n,]/;
+    let numString = numbers;
+
+    if (numbers.startsWith("//")) {
+        const [delimiterPart, ...numberParts] = numbers.split('\n');
+        delimiter = new RegExp(delimiterPart.slice(2));
+        numString = numberParts.join('\n');
+    }
+
+    const numberArr = numString.split(delimiter).map(num => parseInt(num, 10));
     return numberArr.reduce((sum, num) => sum + num, 0);
 }
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -20,3 +20,8 @@ test('should handle new lines between numbers', () => {
     expect(stringCal("1\n2,3")).toBe(6);
 });
 
+test('should support different delimiters', () => {
+    expect(stringCal("//;\n1;2")).toBe(3);
+});
+
+
