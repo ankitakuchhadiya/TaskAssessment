6ed11fb3dc5249cffc00a77e7f270065749ffd7f
ankitakuchhadiya <ankitakuchhadiya@gmail.com>
2024-07-05 16:33:12 +0545

    Handle multiple numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,8 @@ function stringCal(numbers) {
     if (numbers === "") {
         return 0;
     }
-    return parseInt(numbers, 10);
+    const numberArr = numbers.split(',').map(num => parseInt(num, 10));
+    return numberArr.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = stringCal;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,16 @@ const stringCal = require('./stringCalculator');
 test('should return 0 for an empty string', () => {
     expect(stringCal("")).toBe(0);
 });
+
+test('should return the number itself when a single number is provided', () => {
+    expect(stringCal("1")).toBe(1);
+});
+
+test('should return the sum of two numbers', () => {
+    expect(stringCal("1,2")).toBe(3);
+});
+
+test('should return the sum of multiple numbers', () => {
+    expect(stringCal("1,2,3")).toBe(6);
+});
+
